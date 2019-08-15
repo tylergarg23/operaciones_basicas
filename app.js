0c51a5f21d65299029f16c1a571efdf9bfb499b7
tyler26_ <tylergarg@gmail.com>
2019-08-15 15:55:59 -0500

    operaciones basicas

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const sumar = (x, y) =>{
 }
 
 const restar = (x, y) =>{
-	return x + y;
+	return x - y;
 }
 
 const multiplicar = (x, y) =>{
-	return x + y;
+	return x * y;
+}
+
+const dividir = (x, y) =>{
+	return x / y;
 }
